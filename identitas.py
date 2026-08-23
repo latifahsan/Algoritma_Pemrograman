f054a64bb6c22b34a2ffe3649f7bdf45c2c6002f
latifahsan <latifjauhari04@gmail.com>
2026-08-23 20:09:44 +0700

    Add files via upload

---
+++ identitas.py
@@ -0,0 +1,17 @@
+# Di bawah ini
+# adalah data
+program = "Program Identitas"
+nama = "Muhammad Latif Ahsan Jauhari"
+nim = 2605060045
+ttl = "Magelang, 04 Desember 2007"
+jk = "Laki-laki"
+hobi = "Menonton film"
+
+# Di bawah ini
+# adalah output
+print("\t\""+program+"\"\n") # Mencetak Nama Program dengan teks yang diapit tanda petik, tab 1 kali, dan jarak baris kebawah 1 kali 
+print("Nama:", nama) # Mencetak Nama Lengkap
+print("NIM:", nim) # Mencetak NIM
+print("Tempat, Tanggal Lahir:", ttl) # Mencetak Tempat dan Tanggal Lahir
+print("Jenis Kelamin:", jk) # Mencetak Jenis Kelamin
+print("Hobi:", hobi) # Mencetak Hobi
---
+++ pertemuan_1.py
@@ -0,0 +1,16 @@
+#ini adalah komentar satu baris
+print("Hello, World!") #komentar di samping kode
+
+# komentar multi baris
+# ini adalah komentar
+# yang terdiri dari
+# beberapa baris
+
+
+print("Hello, World!");
+
+print("\t\tHalo Apa Kabar?");
+print("\n Selamat datang di \nLab. Teknologi Informasi");
+print("\n Universitas Tidar");
+print("latif\b\bahsan");
+print("\"latif ahsan\"");
